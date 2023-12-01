e53caeafc6cd2a7dc4cfa14a3148988f5f4c602e
Gaurav M <gauravmanchanda16@gmail.com>
2023-12-02 01:38:46 +0530

    Add ruby solution to day one

---
+++ 2023/day_one/main.rb
@@ -0,0 +1,36 @@
+class DayOne
+  NUM_MAP = {
+    "one"=>"one1one",
+    "two"=>"two2two",
+    "three"=>"three3three",
+    "four"=>"four4four",
+    "five"=>"five5five",
+    "six"=>"six6six",
+    "seven"=>"seven7seven",
+    "eight"=>"eight8eight",
+    "nine"=>"nine9nine"
+  }
+
+  LINES = File.readlines("../inputs/day_one.txt")
+
+  class << self
+    def part_one
+      LINES.collect do |line|
+        digits = line.scan(/\d/)
+
+          calibration_value = "#{digits.first}#{digits.last}".to_i
+      end.sum
+    end
+
+    def part_two
+      LINES.collect do |line|
+        NUM_MAP.each do |reg, val|
+          line.gsub!(reg, val)
+        end
+
+        digits = line.scan(/\d/)
+        calibration_value = "#{digits.first}#{digits.last}".to_i
+      end.sum
+    end
+  end
+end
